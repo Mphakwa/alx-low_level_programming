d52f9d6a54c6f1116a529902be4002361883345c
Mphakwa <Mphakwa@outlook.com>
2023-07-14 09:01:20 -0700

    0x01. C - Variables, if, else, while

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - prints all possible combinations of two two-digit numbers
+ * main - Prints all possible combinations of two two-digit numbers
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -14,12 +15,12 @@ int main(void)
 		{
 			if (i < j)
 			{
-				putchar((i / 10) + 48);
-				putchar((i / 10) + 48);
+				putchar((i / 10) + '0'); /* Prints tens digit of i */
+				putchar((i % 10) + '0'); /* Prints ones digit of i */
 				putchar(' ');
-				putchar((j / 10) + 48);
-				putchar((j / 10) + 48);
-				if ( i != 98 || j != 99)
+				putchar((j / 10) + '0'); /* Prints tens digit of j */
+				putchar((j % 10) + '0'); /* Prints ones digit of j */
+				if (i != 98 || j != 99)
 				{
 					putchar(',');
 					putchar(' ');

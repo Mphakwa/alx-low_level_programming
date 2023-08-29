b93c282fcce3944f3a78536b42b2231b876ab97f
Mphakwa <Mphakwa@outlook.com>
2023-08-29 08:35:03 -0700

    0x13. C - More singly linked lists

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -10,7 +10,7 @@
 
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	listint_t *new_node = malloc(sizeof(listint_t));
+	listint_t *new_node = (listint_t *) malloc(sizeof(listint_t));
 
 	if (new_node == NULL)
 	{
@@ -31,7 +31,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 
 		while (current->next != NULL)
 		{
-			current->next = new_node;
+			current = current->next;
 		}
 		current->next = new_node;
 	}

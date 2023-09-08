cd031039725fcf570ce0ea4b97972ff17b530490
Mphakwa <Mphakwa@outlook.com>
2023-09-08 01:33:04 -0700

    0x15. C - File I/O

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,56 +1,97 @@
 #include "main.h"
 
+#define BUFFER_SIZE 1024
+
 /**
- * main - Copies the contents of a file to another file.
- * @argc: The number of arguments supplied to the program.
- * @argv: An array of pointers to the arguments.
- *
- * Return: 0 on success, or exit with an error code on failure.
+ * print_usage_and_exit - Print usage message and exit with a specific code.
+ * @program_name: The name of the program.
  */
-
-int main(int argc, char *argv[])
+void print_usage_and_exit(const char *program_name)
 {
-	int from, to;
-	char buffer[1024];
-	ssize_t bytes_read, bytes_written;
+	dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", program_name);
+	exit(97);
+}
 
-	if (argc != 3)
+/**
+ * print_error_and_exit - Print an error message and exit with a specific code.
+ * @error_message: The error message.
+ * @file_name: The name of the file.
+ * @exit_code: The exit code.
+ */
+void print_error_and_exit(const char *error_message, const char *file_name, int exit_code)
+{
+	dprintf(STDERR_FILENO, "Error: %s %s\n", error_message, file_name);
+	exit(exit_code);
+}
+
+/**
+ * copy_file - Copy the content of one file to another.
+ * @from_file: The source file name.
+ * @to_file: The destination file name.
+ */
+void copy_file(const char *from_file, const char *to_file)
+{
+	int from_fd, to_fd;
+	ssize_t bytes_read, bytes_written;
+	char buffer[BUFFER_SIZE];
+
+	from_fd = open(from_file, O_RDONLY);
+	if (from_fd == -1)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		return (EXIT_FAILURE);
+		print_error_and_exit("Can't read from file", from_file, 98);
 	}
-	from = open(argv[1], O_RDONLY);
-	if (from == -1)
+
+	to_fd = open(to_file, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (to_fd == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		return (EXIT_FAILURE);
+		close(from_fd);
+		print_error_and_exit("Can't write to", to_file, 99);
 	}
-	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (to == -1)
+
+	while ((bytes_read = read(from_fd, buffer, BUFFER_SIZE)) > 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		close(from);
-		return (EXIT_FAILURE);
-	}
-	while ((bytes_read = read(from, buffer, sizeof(buffer))) > 0)
+		bytes_written = write(to_fd, buffer, bytes_read);
+
+	if (bytes_written == -1)
 	{
-		bytes_written = write(to, buffer, bytes_read);
-		if (bytes_written == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		close(from);
-		close(to);
-		return (EXIT_FAILURE);
+		close(from_fd);
+		close(to_fd);
+		print_error_and_exit("Can't write to", to_file, 99);
+		}
 	}
-	}
+
 	if (bytes_read == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		close(from);
-		close(to);
-		return (EXIT_FAILURE);
+		close(from_fd);
+		close(to_fd);
+		print_error_and_exit("Can't read from file", from_file, 98);
 	}
-	close(from);
-	close(to);
-	return (EXIT_SUCCESS);
+
+	if (close(from_fd) == -1)
+	{
+		print_error_and_exit("Can't close fd", from_file, 100);
+	}
+
+	if (close(to_fd) == -1)
+	{
+		print_error_and_exit("Can't close fd", to_file, 100);
+	}
+}
+
+/**
+ * main - Entry point of the program.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ * Return: 0 on success, or an appropriate error code on failure.
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		print_usage_and_exit(argv[0]);
+	}
+
+	copy_file(argv[1], argv[2]);
+
+	return (0);
 }
